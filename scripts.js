89beaa21fa3c8e1e4873f9418fb61452f9859896
Kaleb <kaleb.taylor1976@gmail.com>
2023-03-25 19:45:37 -0700

    add scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Get input elements
+const fromCurrency = document.getElementById('from-currency');
+const fromAmount = document.getElementById('from-amount');
+const toCurrency = document.getElementById('to-currency');
+const toAmount = document.getElementById('to-amount');
